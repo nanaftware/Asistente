e5453ce33f68862744a1ff966130634b6afc7907
RacoonRoach <139692994+nanaftware@users.noreply.github.com>
2025-04-28 19:28:05 -0300

    Create comandos.py
    comandos.py lista los comandos.

---
+++ comandos.py
@@ -0,0 +1,17 @@
+comandos_aplicaciones = [
+    "bloc de notas",
+    "chrome",
+    "brave",
+    "whatsapp",
+    "telegram",
+    "spotify",
+    "apagar computadora",
+    "subir volumen",
+    "bajar volumen"
+]
+
+comandos_detener = [
+    "stop",
+    "detener",
+    "parar"
+]
