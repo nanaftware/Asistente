0c1fe02b98cfc084de13f7c9d40062e89fd51cc4
RacoonRoach <139692994+nanaftware@users.noreply.github.com>
2025-04-29 11:20:48 -0300

    Update comando.py

--- comando.py
+++ comando.py
@@ -1,9 +1,8 @@
 import subprocess
 from audio import responder
 
-def ejecutar_comando(comando):
+def ejecutar(comando: str):
     try:
-        # aplicaciones
         if comando == "bloc de notas":
             subprocess.run("notepad", shell=True)
             responder("Abriendo el bloc de notas.")
@@ -13,32 +12,41 @@ def ejecutar_comando(comando):
         elif comando == "brave":
             subprocess.run("start brave", shell=True)
             responder("Abriendo buscador Brave.")
-        elif comando == "whatsapp":
-            subprocess.run("start whatsapp", shell=True)
-            responder("Abriendo WhatsApp.")
-        elif comando == "telegram":
-            subprocess.run("start telegram", shell=True)
-            responder("Abriendo Telegram.")
-        elif comando == "spotify":
-            subprocess.run("start spotify", shell=True)
-            responder("Abriendo Spotify.")
-        # apagar
+        elif comando == "microsoft edge":   
+            subprocess.run("start msedge", shell=True)
+            responder("Abriendo Microsoft Edge.")
+        elif comando == "explorador de archivos":
+            subprocess.run("explorer", shell=True)
+            responder("Abriendo el explorador de archivos.")
+        elif comando == "calculadora":
+            subprocess.run("calc", shell=True)
+            responder("Abriendo la calculadora.")
+        elif comando == "netflix":
+            subprocess.run("start netflix", shell=True)
+            responder("Abriendo Netflix.")
+        elif comando in ("whatsapp", "telegram", "spotify"):
+            subprocess.run(f"start {comando}", shell=True)
+            responder(f"Abriendo {comando.capitalize()}.")
         elif comando == "apagar computadora":
             responder("Apagando la computadora.")
             subprocess.run("shutdown /s /t 1", shell=True)
-        # volumen
         elif comando == "subir volumen":
             subprocess.run(
-                'powershell -command "(New-Object -ComObject Wscript.Shell).SendKeys([char]175)"', shell=True
+                'powershell -command "(New-Object -ComObject Wscript.Shell).' +
+                'SendKeys([char]175)"', shell=True
             )
-            responder("Subiendo el volumen papi chulo.")
+            responder("Subiendo el volumen.")
         elif comando == "bajar volumen":
             subprocess.run(
-                'powershell -command "(New-Object -ComObject Wscript.Shell).SendKeys([char]174)"', shell=True
+                'powershell -command "(New-Object -ComObject Wscript.Shell).' +
+                'SendKeys([char]174)"', shell=True
             )
-            responder("Bajando el volumen bonito.")
+            responder("Bajando el volumen.")
+        elif comando == "youtube":
+            subprocess.run("start youtube", shell=True)
+            responder("Abriendo YouTube.")
         else:
             responder("Comando no reconocido.")
     except Exception as e:
-        print(f"Error al ejecutar el comando: {e}")
+        print(f"Error al ejecutar comando: {e}")
         responder("Hubo un error al ejecutar el comando.")
